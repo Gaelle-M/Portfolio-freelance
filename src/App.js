c0114c59d430fcc173577b849932805ace4ceba2
Gaelle-M <gaelle-mazzali@hotmail.fr>
2023-05-10 19:35:32 +0200

    modification css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
-import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Formulaire } from "./components/Formulaire";
 import { Footer } from "./components/Footer";
